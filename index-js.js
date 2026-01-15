300fb28de697ff47692bd1a79022bf3ef8e53fb0
sukmarizki04 <sukmarizki04@gmail.com>
2026-01-15 23:17:01 +0700

    message from Cryfrin

--- index-js.js
+++ index-js.js
@@ -1,4 +1,5 @@
-import { createWalletClient, custom, createPublicClient } from "https://esm.sh/viem"
+import { createWalletClient, custom, createPublicClient, parseEther, defineChain } from "https://esm.sh/viem"
+import { contractAddress, coffeAbi } from "./constants-js";
 
 const connectButton = document.getElementById("connectButton");
 const fundButton = document.getElementById("fundButton");
@@ -6,6 +7,8 @@ const ethAmountInput = document.getElementById("ethAmount");
 
 let walletClient
 let publicClient
+
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         walletClient = createWalletClient({
@@ -27,18 +30,45 @@ async function fund() {
         walletClient = createWalletClient({
             transport: custom(window.ethereum)
         })
-        await walletClient.requestAddresses()
+
+        const [connectedAccount] = await walletClient.requestAddresses()
+        const currentChain = await getCurrentChain(walletClient)
         publicClient = createPublicClient({
             transport: custom(window.ethereum)
         })
         await publicClient.simulateContract({
-            address: custom.address
+            address: contractAddress,
+            abi: coffeAbi,
+            functionName: "fund",
+            account: connectedAccount,
+            chain: currentChain,
+            value: parseEther(ethAmount),
         })
 
     } else {
 
         connectButton.innerHTML = "Please Install Metamask!"
     }
+}
+
+
+async function getCurrentChain(client) {
+    const chainId = await client.getChainId()
+    const currentChain = defineChain({
+        id: chainId,
+        name: "Custom Chain",
+        nativeCurrency: {
+            name: "Ether",
+            symbol: "ETH",
+            decimals: 18,
+        },
+        rpcUrls: {
+            default: {
+                http: ["http://localhost:8545"],
+            },
+        },
+    })
+    return currentChain
 }
 
 connectButton.onclick = connect
